e7c309e6d939e2f0f3aeeac8f30cc006b286c486
Muhammed Jaseem Pallikkal <jaseemseems@gmail.com>
2021-11-25 22:44:20 +0300

    added student searching

--- frontend/src/pages/Students.js
+++ frontend/src/pages/Students.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FiSearch, FiUserPlus } from "react-icons/fi";
-import { Button, Form, FormGroup, Input, FormFeedback } from "reactstrap";
+import { FiPenTool, FiSearch, FiUserPlus } from "react-icons/fi";
+import { MdDelete, MdEdit } from "react-icons/md";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  FormFeedback,
+  Label,
+} from "reactstrap";
 import { Spinner } from "react-spinners";
 
 const Students = () => {
@@ -9,28 +17,74 @@ const Students = () => {
   const [searching, setSearching] = useState(false);
   const [errorText, setErrorText] = useState(false);
   const [student, setStudent] = useState(null);
+  const [editing, setEditing] = useState(false);
+  const [deleting, setDeleting] = useState(false);
+
+  const updateValue = (colName, val) => {
+    setStudent({ ...student, [colName]: val });
+    console.log(student);
+  };
+
   const searchStudent = async (e) => {
     e.preventDefault();
     setSearching(true);
-    let res = await fetch(process.env.REACT_APP_API_URL + "/users/login", {
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-      credentials: "include",
-      body: JSON.stringify({
-        reg_no: regNo,
-      }),
+    setStudent({
+      regNo: "B190703CS",
+      name: "Muhammed Jaseem Pallikkal",
+      email: "muhammedjaseem_b190703cs@nitc.ac.in",
+      mobile: "7511135725",
     });
-    if (res.ok) {
-      let student = await res.json();
-      console.log(student);
-      setStudent(student);
-    } else {
-      setErrorText(true);
-      setStudent(null);
-    }
+    // let res = await fetch(process.env.REACT_APP_API_URL + "/users/" + regNo, {
+    //   headers: { "Content-Type": "application/json" },
+    //   method: "GET",
+    //   credentials: "include",
+    //   body: JSON.stringify({
+    //     reg_no: regNo,
+    //   }),
+    // });
+    // if (res.ok) {
+    //   let student = await res.json();
+    //   console.log(student);
+    //   setStudent(student);
+    // } else {
+    //   setErrorText(true);
+    //   setStudent(null);
+    // }
     setSearching(false);
     return;
   };
+
+  const editStudent = async () => {
+    setEditing(true);
+    // let res = await fetch(process.env.REACT_APP_API_URL + "/users/" + regNo, {
+    //   headers: { "Content-Type": "application/json" },
+    //   method: "PUT",
+    //   credentials: "include",
+    //   body: JSON.stringify(student),
+    // });
+    // if (res.ok) {
+    // } else {
+    //   setErrorText(true);
+    // }
+    setEditing(false);
+    return;
+  };
+  const deleteStudent = async () => {
+    setDeleting(true);
+    // let res = await fetch(process.env.REACT_APP_API_URL + "/users/" + regNo, {
+    //   headers: { "Content-Type": "application/json" },
+    //   method: "DELETE",
+    //   credentials: "include",
+    // });
+    // if (res.ok) {
+    // } else {
+    //   setErrorText(true);
+    // }
+    setStudent(null);
+    setDeleting(false);
+    return;
+  };
+
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center mb-3">
@@ -46,8 +100,8 @@ const Students = () => {
           <FiUserPlus size="25" />
         </Link>
       </div>
-      <div className="d-flex justify-content-center shadow rounded">
-        <Form className=" p-4 col-md-5" onSubmit={searchStudent}>
+      <div className="d-flex w-100 flex-column justify-content-center align-items-center shadow rounded p-3 mb-3">
+        <Form className="col-12 col-md-3" onSubmit={searchStudent}>
           <FormGroup>
             <h5 className="text-center mb-3">Search for a student</h5>
             <Input
@@ -66,7 +120,7 @@ const Students = () => {
           </FormGroup>
           <FormGroup className="text-center">
             {searching ? (
-              <Spinner />
+              <Spinner size="25" />
             ) : (
               <Button className="btn btn-primary">
                 <FiSearch size="25" />
@@ -74,6 +128,75 @@ const Students = () => {
             )}
           </FormGroup>
         </Form>
+        {student && (
+          <Form className="rounded card shadow pe-4 ps-4 pt-4 pb-2 col-12 col-md-4">
+            <FormGroup>
+              <Label>Full Name</Label>
+              <Input
+                className="col-lg-6"
+                type="text"
+                value={student.name}
+                placeholder="Full Name"
+                onChange={(e) => {
+                  updateValue("name", e.target.value);
+                }}
+                required
+              ></Input>
+            </FormGroup>
+            <FormGroup>
+              <Label>Reg. No.</Label>
+              <Input
+                className="col-lg-6"
+                type="text"
+                value={student.regNo}
+                placeholder="Reg. No."
+                readOnly
+              ></Input>
+            </FormGroup>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input
+                className="col-lg-6"
+                type="email"
+                value={student.email}
+                placeholder="Reg. No. / Email"
+                onChange={(e) => {
+                  updateValue("email", e.target.value);
+                }}
+                required
+              ></Input>
+            </FormGroup>
+            <FormGroup>
+              <Label>Mobile No.</Label>
+              <Input
+                className="col-lg-6"
+                type="number"
+                value={student.mobile}
+                placeholder="Mobile No."
+                onChange={(e) => {
+                  updateValue("mobile", e.target.value);
+                }}
+                required
+              ></Input>
+            </FormGroup>
+            <FormGroup className="text-center pt-1">
+              <Button className="btn btn-dark me-3" onClick={editStudent}>
+                {editing ? (
+                  <Spinner size="25" />
+                ) : (
+                  <MdEdit size="25" color="white" />
+                )}
+              </Button>
+              <Button className="btn btn-danger" onClick={deleteStudent}>
+                {deleting ? (
+                  <Spinner size="25" />
+                ) : (
+                  <MdDelete size="25" color="white" />
+                )}
+              </Button>
+            </FormGroup>
+          </Form>
+        )}
       </div>
     </div>
   );
