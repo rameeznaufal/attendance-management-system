eae8f346d172a60dcb0bfa36eef68fa8cf8e5765
Muhammed Jaseem Pallikkal <jaseemseems@gmail.com>
2021-11-25 12:52:36 +0300

    students ui

--- frontend/src/pages/Students.js
+++ frontend/src/pages/Students.js
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FiSearch, FiUserPlus } from "react-icons/fi";
-import { Button, Form, FormGroup, Input } from "reactstrap";
+import { Button, Form, FormGroup, Input, FormFeedback } from "reactstrap";
+import { Spinner } from "react-spinners";
 
 const Students = () => {
   const [regNo, setRegNo] = useState("");
-
+  const [searching, setSearching] = useState(false);
+  const [errorText, setErrorText] = useState(false);
+  const [student, setStudent] = useState(null);
   const searchStudent = async (e) => {
     e.preventDefault();
+    setSearching(true);
+    let res = await fetch(process.env.REACT_APP_API_URL + "/users/login", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify({
+        reg_no: regNo,
+      }),
+    });
+    if (res.ok) {
+      let student = await res.json();
+      console.log(student);
+      setStudent(student);
+    } else {
+      setErrorText(true);
+      setStudent(null);
+    }
+    setSearching(false);
     return;
   };
   return (
@@ -25,28 +46,32 @@ const Students = () => {
           <FiUserPlus size="25" />
         </Link>
       </div>
-      <div className="d-flex justify-content-center">
-        <Form
-          className="shadow p-4 rounded col-md-3 col-lg-4 "
-          onSubmit={searchStudent}
-        >
+      <div className="d-flex justify-content-center shadow rounded">
+        <Form className=" p-4 col-md-5" onSubmit={searchStudent}>
           <FormGroup>
             <h5 className="text-center mb-3">Search for a student</h5>
             <Input
-              className="mb-3"
+              className="col-lg-6"
               type="text"
               value={regNo}
               placeholder="Reg. No. / Email"
               onChange={(e) => {
                 setRegNo(e.target.value);
+                setErrorText(false);
               }}
               required
+              invalid={errorText}
             ></Input>
+            <FormFeedback invalid>Student could not be found</FormFeedback>
           </FormGroup>
           <FormGroup className="text-center">
+            {searching ? (
+              <Spinner />
+            ) : (
               <Button className="btn btn-primary">
-              <FiSearch size="25" />
-          </Button>
+                <FiSearch size="25" />
+              </Button>
+            )}
           </FormGroup>
         </Form>
       </div>
