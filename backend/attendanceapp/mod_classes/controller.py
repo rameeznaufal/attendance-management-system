0bff70aefa6a588ba8b46968d895b1c279c791da
Muhammed Jaseem Pallikkal <jaseemseems@gmail.com>
2021-11-28 23:03:37 +0300

    added adding classes ui

---
+++ backend/attendanceapp/mod_classes/controller.py
@@ -0,0 +1,26 @@
+from flask import request, Blueprint, jsonify
+from flask_jwt_extended import get_jwt, unset_jwt_cookies, create_access_token, get_jwt_identity, jwt_required, set_access_cookies
+from attendanceapp import bcrypt
+from .. import db
+from datetime import datetime, timedelta, timezone
+import json
+
+applet = Blueprint('users', __name__, url_prefix='/api/classes')
+
+
+def myconverter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+
+@applet.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=2880))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        return response
