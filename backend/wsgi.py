32afe3268953f828306a674575feda0c607c5ef5
Muhammed Jaseem Pallikkal <jaseemseems@gmail.com>
2021-12-22 19:36:01 +0300

    changed file name

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,4 @@
-from backend import attendanceapp
-from attendanceapp import create_app
+from backend.attendanceapp import app
+from backend.attendanceapp.app import create_app
  
-attendanceapp = create_app()
+app = create_app()
